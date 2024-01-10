a1b1a81c00a42acd9a6730ed0d2eba3396e96e53
elerunnduvictor <velerunndu@gmail.com>
2024-01-10 14:52:36 -0800

    created an hello world project

---
+++ Personal_information.py
@@ -0,0 +1,5 @@
+import os
+os.system("cls")
+
+#This part code prints a little information about me to terminal.
+print("Hello World! My name is Elerunndu Victor and I am a Senior Software Development Major at BYUI. This is my hello world program for my personal software portfolio project.")
